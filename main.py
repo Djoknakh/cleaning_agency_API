6632c79e4d004bba3b8a27d49c8a4a0738dea112
Djoknakh <jok@mail.ru>
2021-08-16 20:48:24 +0300

    new

---
+++ main.py
@@ -0,0 +1,52 @@
+import json
+from http.server import BaseHTTPRequestHandler
+from http.server import HTTPServer
+import logging
+
+
+def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        httpd.server_close()
+
+
+class HttpGetHandler(BaseHTTPRequestHandler):
+    """Обработчик с реализованным методом do_GET."""
+
+
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write('<html><head><meta charset="utf-8">'.encode())
+        self.wfile.write('<title>Простой HTTP-сервер.</title></head>'.encode())
+        self.wfile.write('<body>Был получен GET-запрос.</body></html>'.encode())
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])  # <--- Gets the size of data
+        post_data = self.rfile.read(content_length)  # <--- Gets the data itself
+        action = header.url
+        dispatcher = Dispatcher(action, post_data)
+        response = dispatcher.run()
+        print(response)
+        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+                     str(self.path), str(self.headers), post_data.decode('utf-8'))
+
+        self._set_response()
+        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+
+        meta_data = json.loads(post_data)
+        phone = meta_data["order"]["tel"]
+        print(self.headers)
+
+
+
+run(handler_class=HttpGetHandler)
